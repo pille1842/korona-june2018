e60f4a596773b0b203373acb7ef9fe85194cf4f5
Eric Haberstroh <gpg@erixpage.de>
2017-02-28 18:50:14 +0100

    Add basic public and authentication layouts
    Add two application layouts: One to be displayed for visitors, and one to be used for forms in the public area concerning authentication, password resets etc. In the future, we'll need two more layouts: "internal" for the member-exclusive area, and "backend" for the administration area.

---
+++ resources/views/layouts/public.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('fraternity.name') }}</title>
+
+    <link href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body class="k-body-public">
+
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+              <li{!! Route::getCurrentRoute()->getPath() == '/' ? ' class="active"' : '' !!}><a href="{{ url('/') }}">Startseite</a></li>
+              @if (Auth::guest())
+                  <li><a href="{{ url('login') }}">Anmelden</a></li>
+              @else
+                  <li><a href="#">Interner Bereich</a></li>
+              @endif
+          </ul>
+        </nav>
+        <h3 class="text-muted"><img src="{{ asset(config('fraternity.zirkel')) }}" alt=""> {{ config('fraternity.name') }}</h3>
+      </div>
+
+      @yield('content')
+
+      <footer class="footer">
+        <p>&copy; {{ date('Y') }} {{ config('fraternity.name') }}</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
