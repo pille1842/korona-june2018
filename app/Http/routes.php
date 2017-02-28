ccca9767141046cd24301780f4706fa013d21858
Eric Haberstroh <gpg@erixpage.de>
2017-02-28 17:23:02 +0100

    Clean-up routes and add authentication
    Remove unneccessary comments and add the routes for authentication.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Authentication routes
+Route::post('login', 'Auth\\AuthController@login');
+Route::get('login', 'Auth\\AuthController@showLoginForm');
+Route::get('logout', 'Auth\\AuthController@logout');
+Route::post('password/email', 'Auth\\PasswordController@sendResetLinkEmail');
+Route::post('password/reset', 'Auth\\PasswordController@reset');
+Route::get('password/reset/{token?}', 'Auth\\PasswordController@showResetForm');
