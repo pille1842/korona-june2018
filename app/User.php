8cc04a5acbe9cae9cbdc93d3da56fd9e36c3e441
Eric Haberstroh <gpg@erixpage.de>
2017-03-01 13:46:07 +0100

    Add roles and permissions
    Pull in the bican/roles package and add its Service Provider, middlewares, migrations etc.

--- app/User.php
+++ app/User.php
@@ -3,10 +3,37 @@
 namespace Korona;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Bican\Roles\Traits\HasRoleAndPermission;
+use Bican\Roles\Contracts\HasRoleAndPermission as HasRoleAndPermissionContract;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasRoleAndPermissionContract
 {
+    use HasRoleAndPermission;
+
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function getShortName()
+    {
+        if ($this->nickname != '') {
+            return $this->nickname;
+        } else {
+            return $this->firstname . ' ' . $this->lastname . ' s.n.';
+        }
+    }
+
+    public function getFullName()
+    {
+        if ($this->nickname != '') {
+            return $this->firstname . ' ' . $this->lastname . ' v ' . $this->nickname;
+        } else {
+            return $this->firstname . ' ' . $this->lastname . ' s.n.';
+        }
+    }
+
+    public function getCivilName()
+    {
+        return $this->firstname . ' ' . $this->lastname;
+    }
 }
