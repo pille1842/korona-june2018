396cf5fb7130d15f634cbc85c14e9af4a48f37ef
Eric Haberstroh <gpg@erixpage.de>
2017-05-05 21:13:44 +0200

    Add FontAwesome
    Add FontAwesome to the bower dependencies and add the CSS to all views so that we can use the awesome icons.

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,6 +5,7 @@
 <title>{{ config('fraternity.name') }}</title>
 
 <link href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('bower_components/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 @yield('stylesheets')
