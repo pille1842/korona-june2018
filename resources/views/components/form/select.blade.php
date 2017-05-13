7559573d81dc35854b0bac3cc3b57a61309802c3
Eric Haberstroh <gpg@erixpage.de>
2017-05-13 19:01:45 +0200

    Fix an error in the select form component
    When a select is a multi-select, its name must end with [] to mark it as an array. This fix calls Form::select with the correct element name when a select is marked as "multiple".

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,6 +1,10 @@
 <div class="form-group{{ $errors->has($name) ? ' has-error' : '' }}">
     {{ Form::label($name, trans("validation.attributes.$name"), ['class' => 'control-label']) }}
-    {{ Form::select($name, $options, $value, array_merge(['class' => 'form-control selectpicker', 'aria-describedby' => $name.'HelpBlock'], $attributes)) }}
+    @if (isset($attributes['multiple']) && $attributes['multiple'] == true)
+        {{ Form::select($name.'[]', $options, $value, array_merge(['class' => 'form-control selectpicker', 'aria-describedby' => $name.'HelpBlock', 'id' => $name], $attributes)) }}
+    @else
+        {{ Form::select($name, $options, $value, array_merge(['class' => 'form-control selectpicker', 'aria-describedby' => $name.'HelpBlock'], $attributes)) }}
+    @endif
     @if($errors->has($name))
         <span id="{{ $name }}HelpBlock" class="help-block">
             {{ $errors->first($name) }}
