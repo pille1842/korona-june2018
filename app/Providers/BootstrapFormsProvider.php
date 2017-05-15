fe4b62c0d67cce35f3b8551a235632ccffde9c8a
Eric Haberstroh <gpg@erixpage.de>
2017-05-15 22:55:15 +0200

    Add a textarea component

--- app/Providers/BootstrapFormsProvider.php
+++ app/Providers/BootstrapFormsProvider.php
@@ -17,6 +17,7 @@ class BootstrapFormsProvider extends ServiceProvider
         Form::component('bsCheckbox', 'components.form.checkbox', ['name', 'value' => null, 'checked' => false, 'label' => null]);
         Form::component('bsEmail', 'components.form.email', ['name', 'value' => null, 'attributes' => [], 'label' => null]);
         Form::component('bsText', 'components.form.text', ['name', 'value' => null, 'attributes' => [], 'label' => null]);
+        Form::component('bsTextarea', 'components.form.textarea', ['name', 'value' => null, 'attributes' => [], 'label' => null]);
         Form::component('bsPassword', 'components.form.password', ['name', 'attributes' => [], 'label' => null]);
         Form::component('bsSlug', 'components.form.slug', ['name', 'value' => null, 'source', 'attributes' => [], 'label' => null]);
         Form::component('bsSelect', 'components.form.select', ['name', 'options', 'value' => null, 'attributes' => [], 'label' => null]);
