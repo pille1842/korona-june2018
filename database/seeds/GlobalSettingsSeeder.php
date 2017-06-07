0bf5544138820342ffa3f2e89966ce36974e596e
Eric Haberstroh <eric@erixpage.de>
2017-06-07 14:35:17 +0200

    Correctly seed CSV system settings
    When seeding settings of type 'csv', the value needs to be exploded before adding it to the database.

--- database/seeds/GlobalSettingsSeeder.php
+++ database/seeds/GlobalSettingsSeeder.php
@@ -17,8 +17,13 @@ class GlobalSettingsSeeder extends Seeder
             $prefix = $group['name'] . '.';
 
             foreach ($group['settings'] as $setting) {
+                $value = $setting['value'];
+                if ($setting['type'] == 'csv') {
+                    $value = explode(',', $value);
+                }
+
                 settings([
-                    $prefix.$setting['name'] => $setting['value']
+                    $prefix.$setting['name'] => $value
                 ]);
             }
         }
